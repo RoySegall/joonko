c827b5efb39a11491bcc198e5c45c57fcee87058
RoySegall <roy@segall.io>
2021-04-19 17:46:10 +0300

    Add the contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,27 @@
+import "./contact.scss";
+
 export default () => {
 
-  return <section>
-    Contact
+  return <section className="contact">
+    <div className="titles">
+      <p className="big-title">Want to get the full version?</p>
+      <p className="small-title">Fill in the form below:</p>
+    </div>
+
+    <div className="form">
+      <input placeholder="Full name"/>
+      <input placeholder="Company name"/>
+      <input placeholder="Phone"/>
+      <input placeholder="Work Email"/>
+    </div>
+
+    <div className="actions">
+      <a className="button">Download now >></a>
+    </div>
+
+    <div className="terms">
+      <input type="checkbox" /> <p>I agree to the privacy policy including for Joonko to use my contact details to contact me for marketing purposes.</p>
+    </div>
+
   </section>
 }
